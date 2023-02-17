87b04f2723255d41d414645de751093f8244a1cd
viet_ant <tranviet1101@outlook.com.vn>
2023-02-17 23:02:18 +0700

    new_version

---
+++ Providerss/pub_provider/android/app/src/main/kotlin/com/example/pub_provider/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.pub_provider
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ Providerss/pub_provider/lib/main.dart
@@ -0,0 +1,92 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:pub_provider/state.dart';
+
+void main() {
+  runApp(MultiProvider(
+    providers: [
+      ChangeNotifierProvider(create: (_) => Counter()),
+    ],
+    child: MyApp(),
+  ));
+}
+
+//Todo: Provider
+class Counter with ChangeNotifier {
+  int _count = 0;
+
+  //Todo-->getCount
+  int get count => _count;
+
+  void increment() {
+    _count++;
+    notifyListeners();
+  }
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: MyHomePages(),
+    );
+  }
+}
+
+class MyHomePages extends StatelessWidget {
+  const MyHomePages({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Provider Example'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            const Text("Hay Nhan + De Tang Bo Dem Thoi Gian"),
+            const SizedBox(height: 10),
+            Text(
+              '${context.watch<Counter>().count}',
+              key: const Key('CounterState'),
+              style: Theme.of(context).textTheme.headlineMedium,
+            ),
+            ElevatedButton(
+              onPressed: () {
+                var route = MaterialPageRoute(builder: (context) => MyWidget());
+                Navigator.push(context, route);
+              },
+              child: const Icon(Icons.add_business),
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => context.read<Counter>().increment(),
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
+
+class MyWidget extends StatelessWidget {
+  const MyWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Data Map'),
+      ),
+      body: Center(
+        child: Text('${context.watch<Counter>().count}'),
+      ),
+    );
+  }
+}
